22488e0103ea701cba76cd0ff192744f2dab7bc2
魏志伟 <weizhiwei@100tal.com>
2019-01-21 01:06:14 +0800

    x

--- apollo.php
+++ apollo.php
@@ -4,7 +4,8 @@
    $namespace=$argv[3];
    $key=$argv[4];
 
-   $url='https://apollo.zhiyinlou.com/openapi/v1/envs/{$env}/apps/{$app}/clusters/default/namespaces/{$namespace}';
+
+   $url="https://apollo.zhiyinlou.com/openapi/v1/envs/{$env}/apps/{$app}/clusters/default/namespaces/{$namespace}";
    $headers = array(
                  'Authorization:'.$key,
                  'Content-Type:'.'application/json;charset=UTF-8',
@@ -17,16 +18,14 @@
    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
    $data = curl_exec($curl);
    curl_close($curl);
+
    $data = json_decode($data,true);
    $config_list=[];
    foreach ($data['items'] as $config){
           $config_list[$config['key']]=$config['value'];
-           //var_dump($config_list);
    }
 
-  // var_dump($config_list['config_files']);
    $config_files=explode("\n",$config_list['config_files']);
-  //var_dump($config_files);
    foreach($config_files as $config_file){
         $config_file=explode('=',$config_file);
         file_put_contents(trim($config_file[1]),$config_list[trim($config_file[0])]);
